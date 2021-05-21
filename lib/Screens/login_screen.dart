0d248c0fb5d18ef033074ea12b39505ab61cc5e9
Dmitriy <Dimamakarov1941@yandex.ru>
2021-05-21 18:35:29 +0300

    update path

---
+++ lib/Screens/login_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
+
+const users = const {
+  'user@ya.ru': '1234',
+};
+
+class LoginScreen extends StatelessWidget {
+  Duration get loginTime => Duration(milliseconds: 2150);
+
+  Future<String> _authUser(LoginData data) {
+    print('Имя: ${data.name}, Пароль: ${data.password}');
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(data.name)) {
+        return 'Пользователя не существует';
+      }
+      if (users[data.name] != data.password) {
+        return 'Неверный пароль!';
+      }
+      return '';
+    });
+  }
+
+  Future<String> _recoverPassword(String name) {
+    print('Имя: $name');
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(name)) {
+        return 'Пользователя не существует';
+      }
+      return '';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterLogin(
+      messages: LoginMessages(
+          usernameHint: 'Имя',
+          passwordHint: 'Пароль',
+          confirmPasswordHint: 'Подтвердить',
+          loginButton: 'Войти',
+          signupButton: 'Регистрация',
+          forgotPasswordButton: 'Забыли пароль?',
+          recoverPasswordButton: 'ПОМОГИТЕ',
+          goBackButton: 'ВЕРНУТЬСЯ',
+          confirmPasswordError: 'Не подходит!',
+          recoverPasswordDescription: 'Введите свою почту',
+          recoverPasswordSuccess: 'Пароль успешно изменен',
+          flushbarTitleError: "Ошибка"),
+      title: 'Задачи',
+      // logo: 'assets/images/ecorp-lightblue.png',
+      // emailValidator: (value) => "Неверное имя!",
+      // passwordValidator: (value) => "Неверный пароль!",
+      onLogin: _authUser,
+      onSignup: _authUser,
+      onSubmitAnimationCompleted: () {
+        Navigator.of(context).pushReplacement(MaterialPageRoute(
+          builder: (context) => Container(),
+        ));
+      },
+      onRecoverPassword: _recoverPassword,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:sheremetyevo_tasks/Screens/login_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -30,6 +30,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return LoginScreen();
   }
 }
