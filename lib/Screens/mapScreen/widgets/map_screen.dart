19bd4ec762ff12cfe0a45341384508677b871566
Dmitriy <Dimamakarov1941@yandex.ru>
2021-05-22 13:44:05 +0300

    update map

--- lib/Screens/mapScreen/widgets/map_screen.dart
+++ lib/Screens/mapScreen/widgets/map_screen.dart
@@ -4,10 +4,32 @@ import 'package:photo_view/photo_view.dart';
 class MapScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final ButtonStyle style = ElevatedButton.styleFrom();
     return Container(
-        child: PhotoView(
-      backgroundDecoration: BoxDecoration(color: Colors.white),
-      imageProvider: AssetImage("assets/images/shema1opt.jpg"),
+        child: InteractiveViewer(
+      maxScale: 15,
+      minScale: 1,
+      child: Center(
+        child: Stack(children: [
+          Image(image: AssetImage("assets/images/shema1opt.jpg")),
+          Padding(
+            padding: const EdgeInsets.only(left: 200.0, top: 100),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                FractionallySizedBox(
+                  widthFactor: 1,
+                  child: ElevatedButton(
+                    // style: style,
+                    onPressed: () {},
+                    child: Container(width: 1, height: 1),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ]),
+      ),
     ));
   }
 }
